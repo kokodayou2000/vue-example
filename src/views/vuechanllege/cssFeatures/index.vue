37465564fecfee96044f0e6ce2eb50f5c3aa7bee
kokodayou2000 <1611601667@qq.com>
2024-03-12 23:30:11 +0800

    feat: use vue features v-bind

--- src/views/vuechanllege/cssFeatures/index.vue
+++ src/views/vuechanllege/cssFeatures/index.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineOptions({
   name: 'CssFeatures'
 });
+
+const width = ref(10);
 </script>
+
 <template>
-  <div>CssFeatures</div>
+  <div>
+    CssFeatures
+    <div class="w">
+      {{ width }}
+      <el-button @click="width++">左移</el-button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.w {
+  margin-left: v-bind(width + 'px');
+}
+</style>
